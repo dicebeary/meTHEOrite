73938db98fe9f310f47588ed86d082189e08414e
Kristóf Vajda <vajdak92@gmail.com>
2021-09-04 17:50:01 +0200

    [MTH-4] Add refresh logic

--- Metheorite/UI/Screen/Main/MainViewController.swift
+++ Metheorite/UI/Screen/Main/MainViewController.swift
@@ -6,10 +6,61 @@
 //
 
 import UIKit
+import RxSwift
+import Resolver
+
+protocol MainScreenDelegate: AnyObject {
+    func segmentDidChange(index: Int)
+    func sortButtonTapped()
+}
 
 class MainViewController: UIViewController {
+    
+    @Injected var viewModel: MainViewModel
+    
+    weak var screenDelegate: MainScreenDelegate?
+
+    let segmentedControl = UISegmentedControl(items: [Localization.listTitle, Localization.mapTitle])
+
+    let sortButton = UIBarButtonItem(image: UIImage(systemName: "arrow.up.arrow.down"), style: .plain, target: self, action: #selector(sortButtonTapped(_:)))
+    let refreshButton = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .plain, target: self, action: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setup()
+        setupBindings()
+    }
+    
+    @objc private func sortButtonTapped(_ sender: UIBarButtonItem) {
+        screenDelegate?.sortButtonTapped()
+    }
+
+    @objc private func segmentDidChange(_ sender: UISegmentedControl) {
+        screenDelegate?.segmentDidChange(index: sender.selectedSegmentIndex)
+        if sender.selectedSegmentIndex == 0 {
+            self.title = Localization.listTitle
+        } else {
+            self.title = Localization.mapTitle
+        }
+    }
+}
+
+// MARK: - Setup
+private extension MainViewController {
+    func setup() {
+        segmentedControl.addTarget(self, action: #selector(segmentDidChange(_:)), for: .valueChanged)
+        
+        segmentedControl.frame.size = CGSize(width: 240, height: 30)
+        segmentedControl.selectedSegmentIndex = 0
+
+        navigationItem.titleView = segmentedControl
+        navigationItem.leftBarButtonItem = sortButton
+        navigationItem.rightBarButtonItem = refreshButton
+    }
+    
+    func setupBindings() {
+        let input = MainViewModel.Input(refreshButtonTapped: refreshButton.rx.tap)
+        let _ = viewModel.map(from: input)
     }
 }
